df655f76d659ef8dd4b684df9fb7953f4ac79d73
NatanzMeshaal <natanzme7@gmail.com>
2026-04-17 09:42:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            margin-top: 100px;
+            background-color: #f4f4f4;
+        }
+        h1 {
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <h1>Selamat Datang 👋</h1>
+    <p>Website PHP kamu sudah berjalan!</p>
+</body>
+</html>
